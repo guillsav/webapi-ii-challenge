62f3502fd7d323ab2f43659e4e11df652ae4406f
guillsav <guillsav@gmail.com>
2019-04-23 20:26:03 -0400

    refactored the put route handler

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -5,8 +5,10 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   try {
-    const posts = await db.find();
-    res.status(200).json(posts);
+    const posts = await db.find(req.query);
+    if (posts) {
+      res.status(200).json(posts);
+    }
   } catch (error) {
     res.status(500).json({
       error: 'There was an error while saving the post to the database'
@@ -51,7 +53,7 @@ router.post('/', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   try {
     const post = await db.remove(req.params.id);
-    if (post) {
+    if (post.length > 0) {
       res.status(204).end();
     } else {
       res
@@ -63,65 +65,25 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
-// router.delete('/:id', (req, res) => {
-//   const {id} = req.params;
-//   db.remove(id)
-//     .then(post => {
-//       if (!post) {
-//         res
-//           .status(404)
-//           .json({message: 'The post with the specified ID does not exist.'});
-//       } else {
-//         res.status(204).end();
-//       }
-//     })
-//     .catch(() =>
-//       res.status(500).json({error: 'The post could not be removed'})
-//     );
-// });
-
-// router.put('/:id', async (req, res) => {
-//   try {
-//     console.log(req.body.title);
-
-//     if (req.body.title || req.body.contents) {
-//       const editedPost = await db.update(req.params.id, req.body);
-//       res.status(201).json(editedPost);
-//     } else {
-//       res.status(400).json({
-//         errorMessage: 'Please provide title and contents for the post.'
-//       });
-//     }
-//   } catch (error) {
-//     res.status(500).json({
-//       errorMessage: 'The post information could not be modified.'
-//     });
-//   }
-// });
-
-router.put('/:id', (req, res) => {
-  const {id} = req.params;
-  const editedPost = req.body;
-
-  db.update(id, editedPost)
-    .then(post => {
-      if (!post) {
-        res
-          .status(404)
-          .json({message: 'The post with the specified ID does not exist.'});
-      } else if (req.body.title === '' || req.body.contents === '') {
-        res.status(400).json({
-          errorMessage: 'Please provide title and contents for the post.'
-        });
-      } else {
-        res.status(201).json(post);
-      }
-    })
-    .catch(() =>
-      res.status(500).json({
-        errorMessage: 'The post information could not be modified.'
-      })
-    );
+router.put('/:id', async (req, res) => {
+  try {
+    if (req.body.title === '' || req.body.contents === '') {
+      res.status(400).json({
+        errorMessage: 'Please provide title and contents for the post.'
+      });
+    } else {
+      const editedPost = await db.update(req.params.id, req.body);
+      editedPost
+        ? res.status(201).json(editedPost)
+        : res
+            .status(404)
+            .json({message: 'The post with the specified ID does not exist.'});
+    }
+  } catch {
+    res.status(500).json({
+      errorMessage: 'The post information could not be modified.'
+    });
+  }
 });
 
 module.exports = router;
